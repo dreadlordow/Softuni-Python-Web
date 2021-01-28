54cb75f96f26b7ffae281a7bc895434d8c88ef9a
Georgi Pavlov <75260544+gpavlov21@users.noreply.github.com>
2021-01-28 17:38:06 +0200

    Update settings.py

--- RecipesExam/RecipesExam/settings.py
+++ RecipesExam/RecipesExam/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'recipes',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'georgi',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
